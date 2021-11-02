c3360d6c4eb89af5a822275a4f86b6e8dab1e205
JayGurjar <55961505+JayGurjar@users.noreply.github.com>
2021-11-02 13:05:32 -0600

    Working Student View/Edit

--- src/containers/AdminPages/Students.js
+++ src/containers/AdminPages/Students.js
@@ -1,23 +1,49 @@
-import React from "react";
-import { MDBCard,MDBCardHeader, MDBCardBody } from "mdbreact";
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import "./Students.css"
+const API_HOST = "http://localhost:4000";
+const STUDENT_API_URL = `${API_HOST}/students`;
 
-const columns = ["ID","First Name","Last Name","Email id"];
+function App (){
+   const [data, setData]=useState([]);
+   const fetchStudents = ()=>{
+      fetch(`${STUDENT_API_URL}`)
+      .then(res=>res.json())
+      .then(json=>setData(json));
+  }
+  useEffect(()=>{
+      fetchStudents();
+  },[]);
 
-const data=[
-   [1,"Wasif","Walter","wasif@email.com"]
-];
-
-const TableEditable = props => {
-   return(
-      <MDBCard>
-         <MDBCardHeader tag="h3" className="text-center font-weight-bold text-uppercase py-4">
-            Table Editable
-         </MDBCardHeader>
-         <MDBCardBody>
-            {/* <MDBTableEditable data={data} columns={columns} striped bordered/> */}
-         </MDBCardBody>
-      </MDBCard>
-   )
+  return (
+   <div className="container">
+       <h1> Student List </h1>
+       <table>
+           <thead>
+               <tr>
+                   <th>ID</th>
+                   <th>First Name</th>
+                   <th>Last Name</th>
+                   <th className="age"> Age </th>
+                   <th>Email address</th>
+               </tr>
+           </thead>
+           <tbody>
+              {
+                 data.map((item)=>(
+                    <tr key={item.id}>
+                     <td>{item.id}</td>
+                     <td>{item.first_name}</td>
+                     <td> {item.last_name}</td>
+                     <td>{item.age}</td>
+                     <td> {item.email}</td>
+                     </tr>
+                 ))
+              }
+           </tbody>
+         </table>
+      </div>
+  );
 }
 
-export default TableEditable;
+export default App
