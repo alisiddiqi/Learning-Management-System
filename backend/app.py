8fa8f4e4991db224b0d30cf4a544ca571faa6bb9
Ali Siddiqi <60081630+alisiddiqi@users.noreply.github.com>
2021-11-16 19:43:29 -0700

    added

--- backend/app.py
+++ backend/app.py
@@ -28,6 +28,7 @@ def get_names():
         return "success"
     return render_template('testing.html')
 
+""" Get first name, username  """
 @app.route('/students',methods=['GET','POST'])
 def students():
     if request.method == 'GET':
@@ -45,8 +46,7 @@ def students():
         query_parameters = request.args
         # studentID = query_parameters.get('studentID')
         # year = query_parameters.get('year')
-
-        username = json['username']
+        # username = json['username']
         major = json['major']
         studentID = json['studentID']
         year = json['year']
@@ -55,6 +55,53 @@ def students():
         mysql.connection.commit()
         cur.close()
         return jsonify("sucess insert")
-     
+    
+""" DELETE, PATCH, """
+@app.route('/students/<string:stuUser>/profile',methods=["GET","POST"])
+def stuProfile(stuUser):
+    if request.method=='GET':
+        cur=mysql.connection.cursor()
+        cur.execute("select * from user where username=(%s)",(stuUser,))
+        profile=cur.fetchall()
+        response=jsonify(profile)
+        response.status_code=200
+        cur.close()
+        return response
+    
+    if request.method=='POST':
+        cur=mysql.connection.cursor()
+        
+""" /instructor/<string:stuUser>/ 
+alsoHave a role of isTa
+
+"""
+
+""" /instructor/<string:stuUser>/profile
+    alsoHave a role of isTa
+"""
+
+""" /students/<string:stuUser>/home
+    show all the courses student is taking from profile section
+    show course name, and date
+"""
+
+""" 
+    student home page of courses
+    /students/<int:stuID>/course/<string: courseId>
+    show all the courses student is taking from profile section
+    show course instructor, course name
+"""
+
+""" 
+    to get the discussions for a course
+    /students/<string:usrName>/courses/<string: courseId>
+    ask yuekai
+"""
+
+"""
+ to get the grades
+ /students/<string:usrName>/courses/<string: courseId>
+"""  
+   
 if __name__=="__main__":
     app.run(debug=True)
